ab25b936d9343d22e31bf551cfc0bc6d791249f2
LAB01 <LAB01@Lab1-04>
2016-10-25 11:30:20 -0300

    primeiro commit

---
+++ Odontec/src/main/java/br/OdontecApplication.java
@@ -0,0 +1,12 @@
+package br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdontecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdontecApplication.class, args);
+	}
+}
---
+++ Odontec/src/main/java/br/model/TipoDePaciente.java
@@ -0,0 +1,67 @@
+package br.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class TipoDePaciente {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	private String tipo;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((tipo == null) ? 0 : tipo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TipoDePaciente other = (TipoDePaciente) obj;
+		if (id != other.id)
+			return false;
+		if (tipo == null) {
+			if (other.tipo != null)
+				return false;
+		} else if (!tipo.equals(other.tipo))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return tipo;
+	}
+	
+	
+	
+}

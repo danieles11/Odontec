f3637a236eaf4ea1e3ecbe1a1dd1925542d7d5ff
João Breno <breno9714@gmail.com>
2016-10-28 11:42:59 -0300

    Criado o pacote DAO e feito o repositório do tipo de paciente.

---
+++ Odontec/src/main/java/br/model/DAO/TipoDePacienteRepository.java
@@ -0,0 +1,9 @@
+package br.model.DAO;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.model.TipoDePaciente;
+
+public interface TipoDePacienteRepository extends JpaRepository<TipoDePaciente, Long>{ 
+
+}

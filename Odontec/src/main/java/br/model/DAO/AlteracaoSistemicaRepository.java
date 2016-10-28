4733438bc419bfea748907f96dc9c54ac5556d29
João Breno <breno9714@gmail.com>
2016-10-28 11:51:06 -0300

    Criado o repositório para alterações sistêmicas.

---
+++ Odontec/src/main/java/br/model/DAO/AlteracaoSistemicaRepository.java
@@ -0,0 +1,9 @@
+package br.model.DAO;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.model.AlteracaoSistemica;
+
+public interface AlteracaoSistemicaRepository extends JpaRepository<AlteracaoSistemica, Long>{
+
+}

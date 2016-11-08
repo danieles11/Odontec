bfdff68698880aaa9a4da78a3071981bec5dbd83
lab-01 <lab-01@Lab1-03>
2016-11-08 10:39:18 -0300

    Commit Larbac1

---
+++ Odontec/src/main/java/br/Controller/AlteracaoSistemicaController.java
@@ -0,0 +1,20 @@
+package br.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.model.dao.AlteracaoSistemicaRepository;
+
+@Controller
+public class AlteracaoSistemicaController {
+	
+	@Autowired
+	AlteracaoSistemicaRepository asr; 
+	
+	@RequestMapping(value = "/Alteracao")
+	public
+	
+	
+
+}
